22a2a87c2b7fd740f1ef3476d90d3bd3018b109c
ghachem eya <ghachemeya12@gmail.com>
2021-05-06 17:59:52 +0100

    Secoond Commit

--- src/main/java/com/biblio/bibliotheque/controller/CategorieController.java
+++ src/main/java/com/biblio/bibliotheque/controller/CategorieController.java
@@ -4,8 +4,12 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
+@CrossOrigin
 public class CategorieController {
     
-   
+    @RequestMapping("/")
+    public String Home(){
+        return "hello world!";
+    }
 }
